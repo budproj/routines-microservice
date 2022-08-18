9b8879fa1396f36835f16119eba5413fa27e2d3b
Rodrigo Antas <rodr.antas@gmail.com>
2022-08-18 16:46:47 -0300

    fix(rest-healthcheck-controller): removing unnecessary import

--- src/healthcheck.rest.controller.ts
+++ src/healthcheck.rest.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Request } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('/healthcheck')
 export class HealthCheckRestController {
