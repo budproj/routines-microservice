c7688c9524041e629b4d83fce22daf8ec54d6209
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-09-03 22:24:15 -0300

    refactor(routine): rewrite routines service

--- src/services/routines.service.ts
+++ src/services/routines.service.ts
@@ -1,69 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Routine, Prisma } from '@prisma/client';
 
-import { PrismaService } from '../infrastructure/orm/prisma.service';
+export enum Cadence {
+  WEEKLY = 'WEEKLY',
+}
+
+export interface Routine {
+  name: string;
+  cadence: Cadence;
+}
 
 @Injectable()
 export class RoutineService {
-  constructor(private prisma: PrismaService) {}
+  budRoutine: Routine;
 
-  async routine(
-    routineWhereUniqueInput: Prisma.RoutineWhereUniqueInput,
-  ): Promise<Routine | null> {
-    return this.prisma.routine.findUnique({
-      where: routineWhereUniqueInput,
-    });
+  constructor() {
+    this.budRoutine = {
+      name: 'Retrospectiva',
+      cadence: Cadence.WEEKLY,
+    };
   }
 
-  async routines(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.RoutineWhereUniqueInput;
-    where?: Prisma.RoutineWhereInput;
-    orderBy?: Prisma.RoutineOrderByWithRelationInput;
-  }): Promise<Routine[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.routine.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
+  routine(): Routine {
+    return this.budRoutine;
   }
 
-  async createRoutine(data: Prisma.RoutineCreateInput): Promise<Routine> {
-    return this.prisma.routine.create({
-      data,
-    });
-  }
-
-  async updateRoutine(params: {
-    where: Prisma.RoutineWhereUniqueInput;
-    data: Prisma.RoutineUpdateInput;
-  }): Promise<Routine> {
-    const { data, where } = params;
-    return this.prisma.routine.update({
-      data,
-      where,
-    });
-  }
-
-  async updateRoutines(params: {
-    where: Prisma.RoutineWhereInput;
-    data: Prisma.RoutineUpdateManyMutationInput;
-  }): Promise<Prisma.BatchPayload> {
-    const { data, where } = params;
-
-    return this.prisma.routine.updateMany({
-      where,
-      data,
-    });
-  }
-
-  async deleteRoutine(where: Prisma.RoutineWhereUniqueInput): Promise<Routine> {
-    return this.prisma.routine.delete({
-      where,
-    });
+  getTimeSpanForAnwser(): number {
+    const weeklyTimespan = 7;
+    return weeklyTimespan;
   }
 }
