f7e41523a04147c40ca17d06bfbaa142deca4d5c
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-09-19 09:51:03 -0300

    feat(settings): schedule messages for routine notification and reminder

--- src/controllers/settings/settings.controller.test.ts
+++ src/controllers/settings/settings.controller.test.ts
@@ -1,21 +1,28 @@
+import { ClientNats, ClientsModule, Transport } from '@nestjs/microservices';
 import { Test } from '@nestjs/testing';
 import { randomUUID } from 'crypto';
 
+import { CronService } from '../../services/cron.service';
 import { RoutineSettingsService } from '../../services/routineSettings.service';
 
 import { SettingsController } from './settings.controller';
 
 describe('Settings Controller', () => {
   let controller: SettingsController;
+  const emitMock = jest.spyOn(ClientNats.prototype, 'emit');
 
   const routineSettingsServiceMock = {
     createRoutineSettings: jest.fn(),
   };
 
   const ModuleRef = Test.createTestingModule({
-    imports: [],
+    imports: [
+      ClientsModule.register([
+        { name: 'NATS_SERVICE', transport: Transport.NATS },
+      ]),
+    ],
     controllers: [SettingsController],
-    providers: [RoutineSettingsService],
+    providers: [RoutineSettingsService, CronService],
   })
     .overrideProvider(RoutineSettingsService)
     .useValue(routineSettingsServiceMock);
@@ -30,11 +37,16 @@ describe('Settings Controller', () => {
   const settings = {
     companyId: randomUUID(),
     disabledTeams: [],
-    cron: '0 * * * 5',
+    cron: '0 0 * * 5',
   };
 
   describe('createSettings', () => {
     it('should save only the routine settings to database', async () => {
+      // Arrange
+      routineSettingsServiceMock.createRoutineSettings.mockResolvedValueOnce(
+        settings,
+      );
+
       // Act
       await controller.createSettings(settings);
 
@@ -45,6 +57,32 @@ describe('Settings Controller', () => {
       expect(routineSettingsServiceMock.createRoutineSettings).toBeCalledWith(
         settings,
       );
+    });
+
+    it('should schedule the routine notifications for the given company', async () => {
+      // Arrange
+      const createdSettings = {
+        id: randomUUID(),
+        ...settings,
+      };
+      routineSettingsServiceMock.createRoutineSettings.mockResolvedValueOnce(
+        createdSettings,
+      );
+
+      // Act
+      await controller.createSettings(settings);
+
+      // Assert
+      expect(emitMock).toBeCalledTimes(2);
+      expect(emitMock).toBeCalledWith('createSchedule', {
+        ...createdSettings,
+        queue: 'routine-notification',
+      });
+      expect(emitMock).toBeCalledWith('createSchedule', {
+        ...createdSettings,
+        queue: 'routine-reminder-notification',
+        cron: '0 0 * * 2',
+      });
     });
 
     it('should return the saved routine settings', async () => {
--- src/controllers/settings/settings.controller.ts
+++ src/controllers/settings/settings.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { Prisma, RoutineSettings } from '@prisma/client';
 
+import { CronService } from '../../services/cron.service';
 import { RoutineSettingsService } from '../../services/routineSettings.service';
 
 @Controller('/settings')
 export class SettingsController {
-  constructor(private routineSettings: RoutineSettingsService) {}
+  constructor(
+    @Inject('NATS_SERVICE') private nats: ClientProxy,
+    private routineSettings: RoutineSettingsService,
+    private cron: CronService,
+  ) {}
 
   @Post()
   async createSettings(
@@ -14,6 +20,24 @@ export class SettingsController {
     const createdSettings = await this.routineSettings.createRoutineSettings(
       settings,
     );
+
+    const routineNotificationData = {
+      ...createdSettings,
+      queue: 'routine-notification',
+    };
+    this.nats.emit('createSchedule', routineNotificationData);
+
+    const daysToRoutineReminder = 3;
+    const routineReminderCron = this.cron.addDaysToCron(
+      createdSettings.cron,
+      daysToRoutineReminder,
+    );
+    const routineReminderNotificationData = {
+      ...createdSettings,
+      queue: 'routine-reminder-notification',
+      cron: routineReminderCron,
+    };
+    this.nats.emit('createSchedule', routineReminderNotificationData);
 
     return createdSettings;
   }
