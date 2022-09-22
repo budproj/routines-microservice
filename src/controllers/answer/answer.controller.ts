e9e4cba530604c2b4e2e89d04c99d14e78360b43
diego magalhaes <digengup@gmail.com>
2022-09-22 09:29:20 -0300

    qa(1.0) questions are only mandatory if not hidden

--- src/controllers/answer/answer.controller.ts
+++ src/controllers/answer/answer.controller.ts
@@ -1,23 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { Answer } from '@prisma/client';
+import { Prisma } from '@prisma/client';
+
 import { User } from '../../decorators/user.decorator';
 import { AnswersService } from '../../services/answers.service';
 import { AnswerGroupService } from '../../services/answerGroup.service';
 import { Team } from '../../types/Team';
 import { User as UserType } from '../../types/User';
 import { budRoutinePtBrForm } from '../../shared/providers/form-provider/bud-routine-ptBR';
+import { AnswerWithHiddenFieldInfo } from '../../types/Answer';
 
 @Controller('/answer')
 export class AnswerController {
-  constructor(
-    private answerGroupService: AnswerGroupService,
-    private answerService: AnswersService,
-  ) {}
+  constructor(private answerGroupService: AnswerGroupService) {}
 
   @Post()
   async registerAnswers(
     @User() user: UserType,
-    @Body() answers: Answer[],
+    @Body() answers: AnswerWithHiddenFieldInfo[],
   ): Promise<Team[]> {
     const {
       companies: [company],
@@ -25,35 +24,38 @@ export class AnswerController {
       teams,
     } = user;
 
-    const requiredQuestionsIDs = budRoutinePtBrForm.reduce(
-      (results, { id, required }) => {
-        if (typeof required === 'boolean' && required === true)
-          results.push(id);
-        return results;
+    const requiredQuestionIds = budRoutinePtBrForm
+      .filter((question) => question.required)
+      .map((question) => question.id);
+
+    console.log({ answers });
+
+    const allRequiredAnswersHaveBeenFilled = requiredQuestionIds.every(
+      (requiredQuestionId) => {
+        const answer = answers.find(
+          ({ questionId }) => questionId === requiredQuestionId,
+        );
+        return answer.hidden ? true : Boolean(answer.value);
       },
-      [],
     );
 
-    let matchedMandatoryQuestions = 0;
-    requiredQuestionsIDs.forEach((requiredQuestion) => {
-      for (let i = 0; i < answers.length; i++) {
-        if (answers[i].questionId === requiredQuestion)
-          matchedMandatoryQuestions++;
-      }
-    });
-
-    if (matchedMandatoryQuestions < requiredQuestionsIDs.length) {
-      throw new Error('All mandatory questions must be answered.');
+    if (!allRequiredAnswersHaveBeenFilled) {
+      throw new Error('All required questions must be answered.');
     }
 
-    const { id } = await this.answerGroupService.createAnswerGroup({
+    const submittedAnswers = answers
+      .filter((answer) => !answer.hidden)
+      .map<Prisma.AnswerCreateWithoutAnswerGroupInput>((answer) => ({
+        questionId: answer.questionId,
+        value: answer.value,
+      }));
+
+    await this.answerGroupService.createAnswerGroup({
       companyId: company.id,
       userId,
-    });
-
-    await this.answerService.createAnswer({
-      data: answers.map((answer) => ({ ...answer, answerGroupId: id })),
-      skipDuplicates: true,
+      answers: {
+        create: submittedAnswers,
+      },
     });
 
     return teams;
