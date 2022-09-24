e322727ddd1835f3370fe7918771c82927540c1c
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-09-24 11:21:42 -0300

    feat(security): import security service to application

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { RoutineSettingsService } from './services/routineSettings.service';
 import { AnswerGroupService } from './services/answerGroup.service';
 import { CronService } from './services/cron.service';
 import { FormService } from './services/form.service';
+import { SecurityService } from './services/security.service';
 
 import { UserValidatorMiddleware } from './middlewares/user-validator.middleware';
 import { AppLoggerMiddleware } from './middlewares/route-logger.middleware';
@@ -60,6 +61,7 @@ import configuration from './config/configuration';
     AnswersService,
     CronService,
     FormService,
+    SecurityService,
   ],
 })
 export class AppModule implements NestModule {
