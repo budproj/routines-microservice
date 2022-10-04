dd7aacef96455a845071103aab76bd394323092d
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-10-03 22:34:10 -0300

    fix(type): user id as string

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,7 +1,7 @@
 import { Team } from './Team';
 
 export interface User {
-  id: '922ef72a-6c3c-4075-926a-3245cdeea75f';
+  id: string;
   teams: Team[];
   companies: Team[];
   permissions: string[];
