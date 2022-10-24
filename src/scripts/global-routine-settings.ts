2536d76dfa7c89609dc00b5cc1e422bef2cd3746
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-10-24 20:44:56 -0300

    chore(settings): set the base cron set to 0 0 * * 5

--- src/scripts/global-routine-settings.ts
+++ src/scripts/global-routine-settings.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 
   const baseSettings = {
     disabledTeams: [],
-    cron: '* * * * 5',
+    cron: '0 0 * * 5',
   };
 
   await settings.globalRoutineSettingsCreation(user, baseSettings);
