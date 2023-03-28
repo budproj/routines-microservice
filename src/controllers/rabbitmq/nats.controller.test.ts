168a1142b19a6925043675878c17719730b402b4
Victor Perin <me@victorperin.ninja>
2023-03-28 15:09:52 -0300

    test(controllers/rabbitmq): getAnswerGroupData

--- src/controllers/rabbitmq/nats.controller.test.ts
+++ src/controllers/rabbitmq/nats.controller.test.ts
@@ -23,7 +23,10 @@ describe('NATS Controller', () => {
     removeDisabledTeamsFromUser: jest.fn(),
     getCurrentRoutineDate: jest.fn(),
   };
-  const answerGroupServiceMock = { answerGroups: jest.fn() };
+  const answerGroupServiceMock = {
+    answerGroups: jest.fn(),
+    answerGroup: jest.fn(),
+  };
   const messagingServiceMock = {
     sendMessage: jest.fn(),
     emit: jest.fn(),
@@ -206,6 +209,24 @@ describe('NATS Controller', () => {
         answerGroupServiceMock.answerGroups.mock.calls[0][0].where.timestamp
           .gte,
       ).toEqual(new Date('2022-10-07T00:00:00.000Z'));
+    });
+  });
+
+  describe('getAnswerGroupData', () => {
+    it('should call answerGroupService.answerGroup and reply its content', async () => {
+      // Arrange
+      const payload = { id: 'um id' };
+      const mockedResponse = { an: 'object' };
+      answerGroupServiceMock.answerGroup.mockResolvedValueOnce(mockedResponse);
+
+      // Act
+      const result = await natsController.getAnswerGroupData({ id: 'um id' });
+
+      // Assert
+      expect(1).toBe(1);
+      expect(answerGroupServiceMock.answerGroup).toBeCalledTimes(1);
+      expect(answerGroupServiceMock.answerGroup).toBeCalledWith(payload);
+      expect(result).toBe(mockedResponse);
     });
   });
 });
